df303885dfc25d2632b661e62f0e505c612a32fa
stann1 <stoychev.st2@gmail.com>
2015-02-13 15:10:00 +0200

    added windows service to the solution

---
+++ WindowsServiceHost/TrollStealthService.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace WindowsServiceHost
+{
+    partial class TrollStealthService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventTimer = new System.Windows.Forms.Timer(this.components);
+            this.eventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).BeginInit();
+            // 
+            // eventTimer
+            // 
+            this.eventTimer.Tick += new System.EventHandler(this.eventTimer_Tick);
+            // 
+            // TrollStealthService
+            // 
+            this.ServiceName = "TrollService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer eventTimer;
+        private System.Diagnostics.EventLog eventLog;
+    }
+}
---
+++ WindowsServiceHost/TrollStealthService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceHost
+{
+    public partial class TrollStealthService : ServiceBase
+    {
+        public const int RepetitionTimeInMilliseconds = 3600*1000;
+
+        public TrollStealthService()
+        {
+            InitializeComponent();
+            this.eventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MyTrollSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MyTrollSource", "MyNewTrollLog");
+            }
+            this.eventLog.Source = "MyTrollSource";
+            this.eventLog.Log = "MyNewTrollLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.eventLog.WriteEntry("Troll service started");
+            eventTimer.Interval = RepetitionTimeInMilliseconds;
+            eventTimer.Tick += eventTimer_Tick;
+            eventTimer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            this.eventLog.WriteEntry("Troll service stopped");
+        }
+
+        private void eventTimer_Tick(object sender, EventArgs e)
+        {
+            this.eventLog.WriteEntry("Initiate troll service timer event");
+
+            // TODO: Put the troll code here
+        }
+    }
+}
